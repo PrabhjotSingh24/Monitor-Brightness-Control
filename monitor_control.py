6182de918a14e2fa1620c753f69e2395e81ce16d
Prabhjot Singh <prabhjotsingh2401@gmail.com>
2024-06-29 02:44:25 -0600

    initial commit

---
+++ monitor_control.py
@@ -0,0 +1,4 @@
+import screen_brightness_control as sbc
+
+
+print(sbc.get_brightness())
---
+++ system_tray.py
@@ -0,0 +1,39 @@
+from pystray import Icon as icon, Menu as menu, MenuItem as item
+from PIL import Image, ImageDraw
+import screen_brightness_control as sbc
+from math import floor
+def create_image(width, height, color1, color2):
+    # Generate an image and draw a pattern
+    image = Image.new('RGB', (width, height), color1)
+    dc = ImageDraw.Draw(image)
+    dc.rectangle(
+        (width // 2, 0, width, height // 2),
+        fill=color2)
+    dc.rectangle(
+        (0, height // 2, width // 2, height),
+        fill=color2)
+
+    return image
+state = sbc.set_brightness(floor(sbc.get_brightness()[0]), no_return=False)[0]
+
+def set_state(v):
+    def inner(icon, item):
+        global state
+        state = v
+        sbc.fade_brightness(int(item.text.split()[-1]),logarithmic=True,interval=0.0001,increment=5)
+    return inner
+
+def get_state(v):
+    def inner(item):
+        return state == v
+    return inner
+
+# Let the menu items be a callable returning a sequence of menu
+# items to allow the menu to grow
+icon('test', create_image(64,64,'black','white'), menu=menu(lambda: (
+    item(
+        'Brightness %d' % i,
+        set_state(i),
+        checked=get_state(i),
+        radio=True)
+    for i in range(0,101,5)))).run()
